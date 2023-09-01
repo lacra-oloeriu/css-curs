dcf86b9c5eafac1b43bdb2938f14cdbe85ad68fa
Lacramioara <lacramioara.oloeriu@gmail.com>
2023-09-01 12:27:12 +0200

    added a event listener

--- css/shared.js
+++ css/shared.js
@@ -1,3 +1,14 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
-backdrop.style.display='block'
+//console.dir(selectPlanButtons)
+
+//backdrop.style.display='block'
+
+for(var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
